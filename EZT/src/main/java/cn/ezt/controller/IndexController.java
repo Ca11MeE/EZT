5bb8e6ef2160d72734650a9f462ff56977fcb5f3
37247 <37247@DESKTOP-17LB8MQ>
2017-09-12 00:15:01 +0800

    2017/09/12-00:12
    粗略实现shiro登录,效果不算太满意,特别30天自动登录方面
    
    Signed-off-by: 37247 <37247@DESKTOP-17LB8MQ>

--- EZT/src/main/java/cn/ezt/controller/IndexController.java
+++ EZT/src/main/java/cn/ezt/controller/IndexController.java
@@ -3,10 +3,16 @@ package cn.ezt.controller;
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.authc.AuthenticationException;
+import org.apache.shiro.authc.UsernamePasswordToken;
+import org.apache.shiro.subject.Subject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.mysql.jdbc.StringUtils;
 
 import cn.ezt.pojo.User;
 import cn.ezt.service.UserService;
@@ -16,29 +22,40 @@ public class IndexController {
 
 	@Autowired
 	UserService userService;
-	
+
 	@RequestMapping("/tomain")
-	public String toIndex(HttpServletRequest request,Model model) {
-//		//检查是否有自动登录信息
-//		Cookie[] cookies = request.getCookies();
-//		for (Cookie cookie : cookies) {
-//			if ("autologin".equals(cookie.getName())) {
-//				//有登录信息
-//				String[] info = cookie.getValue().split(":");
-//				String username = info[0];
-//				String password = info[1];
-//				User user = userService.findUserByUP(username, password);
-//				if (user!=null) {
-//					model.addAttribute("user", user);
-//					if ("1".equals(user.getRole().getUserRole() == null ? "" : user.getRole().getUserRole())) {
-//						return "/User/UserInfo";
-//					}
-//
-//					return "redirect:/Admin/AdminInfo.action";
-//				}
-//				break;
-//			}
-//		}
+	public String toIndex(HttpServletRequest request, Model model) {
+		// 检查是否有自动登录信息
+		Cookie[] cookies = request.getCookies();
+		for (Cookie cookie : cookies) {
+			if ("autologin".equals(cookie.getName())) {
+				if (StringUtils.isNullOrEmpty(cookie.getValue())) {
+					return "redirect:/tologin.action";
+				}
+				// 有登录信息
+				String[] info = cookie.getValue().split(":");
+				String username = info[0];
+				String password = info[1];
+				Subject subject = SecurityUtils.getSubject();
+				UsernamePasswordToken token = new UsernamePasswordToken(username, password);
+				try {
+					subject.login(token);
+					User user = (User) subject.getPrincipal();
+					if (user != null) {
+						model.addAttribute("user", user);
+						if ("1".equals(user.getRole().getUserRole() == null ? "" : user.getRole().getUserRole())) {
+							return "/User/UserInfo";
+						}
+
+						return "redirect:/Admin/AdminInfo.action";
+					}
+					break;
+				} catch (AuthenticationException e) {
+					return "redirect:/tologin.action";
+				}
+			}
+		}
 		return "redirect:/tologin.action";
+
 	}
 }
--- EZT/src/main/java/cn/ezt/controller/LoginController.java
+++ EZT/src/main/java/cn/ezt/controller/LoginController.java
@@ -24,7 +24,7 @@ import cn.ezt.service.UserService;
 public class LoginController {
 
 	@Autowired
-	UserService userService;
+	private UserService userService;
 
 	@RequestMapping("/tologin")
 	public String toLogin(HttpSession session) {
@@ -41,93 +41,80 @@ public class LoginController {
 
 	@RequestMapping("/login")
 	public String login(String username, String password, String valistr, String remindUserName, String unlogin,
-			Model model, HttpSession session,HttpServletResponse response) {
-		// 如果已登录则直接跳转到首页
-//		User logined = (User) session.getAttribute("user");
-//		if (logined != null) {
-//			User logineduser = userService.findUserByUP(logined.getUsername(), logined.getPassword());
-//			if (logineduser != null) {
-//				if ("1".equals(
-//						logineduser.getRole().getUserRole() == null ? "" : logineduser.getRole().getUserRole())) {
-//					return "/User/UserInfo";
-//				}
-//
-//				return "redirect:/Admin/AdminInfo.action";
-//			}
-//
-//		}
-		// 检查验证码
-		String realStr = (String) session.getAttribute("valistr");
-		if (realStr == null || !realStr.equalsIgnoreCase(valistr)) {
-			model.addAttribute("errMsg", "验证码不正确");
-			return "/login";
-		}
-		if (StringUtils.isEmpty(username) || StringUtils.isEmpty(password)) {
-			model.addAttribute("errMsg", "用户名或密码不能为空");
-			return "/login";
-		}
-
-//		// 检查是否勾选记住用户名
-//		if (remindUserName != null && "on".equals(remindUserName)) {
-//			// 勾选
-//			model.addAttribute("enteredUserName",username);
-//		}else {
-//			model.addAttribute("enteredUserName",null);
-//		}
-//		// 检查是否勾选30天免登陆
-//		if (unlogin != null && "on".equals(unlogin)) {
-//			// 勾选
-//			Cookie au=new Cookie("autologin", username+":"+password);
-//			au.setMaxAge(30*24*3600);
-//			au.setPath("/");
-//			response.addCookie(au);
-//		}else {
-//			Cookie au=new Cookie("autologin", null);
-//			au.setMaxAge(-1);
-//			au.setPath("/");
-//			response.addCookie(au);
-//		}
-//		session.setAttribute("user", user);
-//		model.addAttribute("user", user);
-		//利用shiro登陆
+			Model model, HttpSession session, HttpServletResponse response) {
+		// 利用shiro登陆
 		Subject subject = SecurityUtils.getSubject();
-		UsernamePasswordToken token=new UsernamePasswordToken(username, password);
+		UsernamePasswordToken token = null;
+		// 如果已登录则直接跳转到首页
+		User logined = (User) session.getAttribute("user");
+		if (logined != null) {
+			token = new UsernamePasswordToken(logined.getUsername(), logined.getPassword());
+
+		} else {
+			// 检查验证码
+			String realStr = (String) session.getAttribute("valistr");
+			if (realStr == null || !realStr.equalsIgnoreCase(valistr)) {
+				model.addAttribute("errMsg", "验证码不正确");
+				return "/login";
+			}
+			if (StringUtils.isEmpty(username) || StringUtils.isEmpty(password)) {
+				model.addAttribute("errMsg", "用户名或密码不能为空");
+				return "/login";
+			}
+
+			// 检查是否勾选记住用户名
+			if (remindUserName != null && "on".equals(remindUserName)) {
+				// 勾选
+				model.addAttribute("enteredUserName", username);
+			} else {
+				model.addAttribute("enteredUserName", null);
+			}
+
+			token = new UsernamePasswordToken(username, password);
+		}
 		try {
-//			subject.login(token);
-			//登陆成功
-//			session.setAttribute("user", (User)subject.getPrincipal());
-			User user = userService.findUserByUP(remindUserName, password);
-			if (user!=null) {
-				
-			Role userRole = ( (User)subject.getPrincipal()).getRole();
+			subject.login(token);
+			// 登陆成功
+			session.setAttribute("user", (User) subject.getPrincipal());
+			Role userRole = ((User) subject.getPrincipal()).getRole();
 			if ("1".equals(userRole.getUserRole() == null ? "" : userRole.getUserRole())) {
+				// 检查是否勾选30天免登陆
+				if (unlogin != null && "on".equals(unlogin)) {
+					// 勾选
+					Cookie au = new Cookie("autologin", username + ":" + password);
+					au.setMaxAge(30 * 24 * 3600);
+					au.setPath("/");
+					response.addCookie(au);
+				} else {
+					Cookie au = new Cookie("autologin", null);
+					au.setMaxAge(-1);
+					au.setPath("/");
+					response.addCookie(au);
+				}
 				return "/User/UserInfo";
 			}
 			return "redirect:/Admin/AdminInfo.action";
-			}else {
-				model.addAttribute("errMsg", "用户名或密码不正确");
-				return "/login";
-			}
+
 		} catch (AuthenticationException e) {
-			//登录失败
-			e.printStackTrace();
+			// 登录失败
 			model.addAttribute("errMsg", "用户名或密码不正确");
 			return "/login";
-		}catch(Exception e) {
-			e.printStackTrace();
+		} catch (Exception e) {
 			model.addAttribute("errMsg", "发生错误,请联系网站管理员解决");
 			return "/login";
 		}
 		// 日后添加根据用户权限加载不同页面
-		
 
-		
 	}
 
 	// 登出操作
 	@RequestMapping("/logout")
-	public String logout(HttpSession session) {
+	public String logout(HttpSession session,HttpServletResponse response) {
 		session.setAttribute("user", null);
+		Cookie au = new Cookie("autologin", null);
+		au.setMaxAge(-1);
+		au.setPath("/");
+		response.addCookie(au);
 		return "redirect:/tologin";
 	}
 
